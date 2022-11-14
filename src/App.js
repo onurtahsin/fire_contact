dcbf3648c6a471d6c4a663a01e0ea0e8f94f4316
onur <kizilayonurtahsin@gmaik.com>
2022-11-14 20:32:17 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+import Contacts from "./components/contacts/Contacts";
+import FormComponent from "./components/form/FormComponent";
+import { useState } from "react";
+
+const initialValues = {
+  username: "",
+  phoneNumber: "",
+  gender: "",
+};
+function App() {
+  const [info, setInfo] = useState(initialValues);
+  return (
+    <div className="App">
+      <FormComponent info={info} setInfo={setInfo} />
+      <Contacts />
+    </div>
+  );
+}
+
+export default App;
